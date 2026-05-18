84b542ed5ef7b23cd346806079faa51a22b1b79c
quangiscoding <quangnguyen2005gt08@gmail.com>
2026-05-18 09:46:41 +0700

    Add basename for BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import { CartProvider } from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/React-Product-Store/">
       <AuthProvider>
         <ThemeProvider>
           <CartProvider>
